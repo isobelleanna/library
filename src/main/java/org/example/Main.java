078b2bb37d2208f47d2529c96be8b0b22603401b
isobelleanna <67749649+isobelleanna@users.noreply.github.com>
2022-08-29 16:33:17 +0100

    read csv file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,16 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("books_data.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext()){
+            System.out.println(scanner.next());
+        }
+        scanner.close();
     }
 }
